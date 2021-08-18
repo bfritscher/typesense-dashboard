988e1abc404e54bbb93966e272667a314fbb26e2
Zierhut-IT <52775840+Zierhut-IT@users.noreply.github.com>
2021-08-18 18:59:57 +0200

    add Dockerfile

---
+++ Dockerfile
@@ -0,0 +1,20 @@
+FROM node:14-alpine as builder
+
+WORKDIR /app
+RUN yarn global add @quasar/cli
+
+COPY package.json package-lock.json yarn.lock ./
+RUN yarn install
+
+COPY . .
+ENV PUBLIC_PATH /
+RUN quasar build
+
+
+FROM caddy:2-alpine
+
+WORKDIR /srv
+COPY --from=builder /app/dist/spa/ ./
+
+EXPOSE 80
+CMD ["caddy", "file-server"]
--- quasar.conf.js
+++ quasar.conf.js
@@ -55,7 +55,7 @@ module.exports = configure(function (ctx) {
     // Full list of options: https://v2.quasar.dev/quasar-cli/quasar-conf-js#Property%3A-build
     build: {
       vueRouterMode: 'hash', // available values: 'hash', 'history'
-      publicPath: process.env.NODE_ENV === 'development' ? '/' : '/typesense-dashboard',
+      publicPath: process.env.PUBLIC_PATH ?? process.env.NODE_ENV === 'development' ? '/' : '/typesense-dashboard',
       // transpile: false,
 
       // Add dependencies for transpiling with Babel (Array of string/regex)
