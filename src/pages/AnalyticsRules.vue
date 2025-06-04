06d9b02f73acb1b87605ca687558dfd958cf1933
Boris Fritscher <boris@fritscher.ch>
2025-06-04 13:23:16 +0200

    feat: add json mode to analytics rules and allow to select aliases fixes  #88

--- src/pages/AnalyticsRules.vue
+++ src/pages/AnalyticsRules.vue
@@ -1,69 +1,99 @@
 <template>
   <q-page padding>
-    <q-expansion-item
-      v-model="state.expanded"
-      expand-separator
-      icon="sym_s_add_circle"
-      expand-icon="sym_s_unfold_more"
-      expanded-icon="sym_s_unfold_less"
-      :label="`${isUpdate ? 'Update' : 'Add'} Analytics Rule`"
-      header-class="bg-primary text-white"
-    >
-      <q-card class="bg-surface column">
-        <q-card-section class="row q-col-gutter-md">
-          <q-input
-            v-model="state.rule.name"
-            class="col-12 col-sm-6"
-            label="Rule Name"
-            filled
-            :rules="[(val) => !!val || 'Field is required']"
-          />
-
-          <q-input
-            v-model="state.rule.type"
-            class="col-12 col-sm-6"
-            label="Rule Type"
-            filled
-            :rules="[(val) => !!val || 'Field is required']"
-          />
-        </q-card-section>
-        <q-card-section class="row q-col-gutter-md">
-          <q-select
-            v-model="state.rule.params.source.collections"
-            class="col-12 col-sm-6"
-            label="Source Collection(s)"
-            filled
-            :options="collectionNames"
-            multiple
-            hint="Track searches sent to these collections"
-          ></q-select>
-
-          <q-select
-            v-model="state.rule.params.destination.collection"
-            class="col-12 col-sm-6"
-            label="Destination Collection"
-            filled
-            :options="collectionNames"
-          ></q-select>
-        </q-card-section>
-        <q-card-section class="row q-col-gutter-md">
-          <q-input
-            v-model="state.rule.params.limit"
-            class="col-12 col-sm-6"
-            label="Limit"
-            filled
-            type="number"
-            min="0"
-          />
-        </q-card-section>
-
-        <q-card-actions align="right" class="bg-primary">
-          <q-btn size="md" padding="sm lg" unelevated color="primary" @click="createRule()"
-            >{{ isUpdate ? 'Update' : 'Add' }} Rule</q-btn
+    <q-list bordered class="rounded-borders">
+      <q-expansion-item
+        v-model="state.expanded"
+        expand-separator
+        icon="sym_s_add_circle"
+        expand-icon="sym_s_unfold_more"
+        expanded-icon="sym_s_unfold_less"
+        :label="`${isUpdate ? 'Update' : 'Add'} Analytics Rule`"
+        header-class="bg-primary text-white"
+      >
+        <q-card>
+          <q-tabs
+            v-model="tab"
+            dense
+            class="text-grey"
+            active-color="primary"
+            indicator-color="primary"
+            align="justify"
+            narrow-indicator
           >
-        </q-card-actions>
-      </q-card>
-    </q-expansion-item>
+            <q-tab name="form" label="Form Mode" />
+            <q-tab name="json" label="JSON Mode" />
+          </q-tabs>
+          <q-separator />
+          <q-tab-panels v-model="tab" animated class="bg-surface">
+            <q-tab-panel name="form">
+              <q-card-section class="row q-col-gutter-md">
+                <q-input
+                  v-model="state.rule.name"
+                  class="col-12 col-sm-6"
+                  label="Rule Name"
+                  filled
+                  :rules="[(val) => !!val || 'Field is required']"
+                />
+                <q-select
+                  v-model="state.rule.type"
+                  :options="typeOptions"
+                  class="col-12 col-sm-6"
+                  label="Rule Type"
+                  filled
+                  :rules="[(val) => !!val || 'Field is required']"
+                />
+              </q-card-section>
+              <q-card-section class="row q-col-gutter-md">
+                <q-select
+                  v-model="state.rule.params.source.collections"
+                  class="col-12 col-sm-6"
+                  label="Source Collection(s)"
+                  filled
+                  :options="sourceOptions"
+                  multiple
+                  hint="Track searches sent to these collections or aliases"
+                ></q-select>
+                <q-select
+                  v-model="state.rule.params.destination.collection"
+                  class="col-12 col-sm-6"
+                  label="Destination Collection"
+                  filled
+                  :options="sourceOptions"
+                ></q-select>
+              </q-card-section>
+              <q-card-section class="row q-col-gutter-md">
+                <q-input
+                  v-model="state.rule.params.limit"
+                  class="col-12 col-sm-6"
+                  label="Limit"
+                  filled
+                  type="number"
+                  min="0"
+                />
+                <q-checkbox
+                  v-model="state.rule.params.expand_query"
+                  class="col-12 col-sm-6"
+                  label="Expand Query"
+                  filled
+                />
+              </q-card-section>
+            </q-tab-panel>
+            <q-tab-panel name="json" class="q-pa-none">
+              <monaco-editor v-model="ruleJson" style="height: 60vh"></monaco-editor>
+              <q-banner v-if="jsonError" inline-actions class="text-white bg-red">
+                Invalid Format: {{ jsonError }}
+              </q-banner>
+            </q-tab-panel>
+          </q-tab-panels>
+          <q-separator />
+          <q-card-actions align="right" class="bg-primary">
+            <q-btn size="md" padding="sm lg" unelevated color="primary" @click="createRule()">
+              {{ isUpdate ? 'Update' : 'Add' }} Rule
+            </q-btn>
+          </q-card-actions>
+        </q-card>
+      </q-expansion-item>
+    </q-list>
 
     <q-table
       class="q-mt-md"
@@ -105,9 +135,10 @@
 <script setup lang="ts">
 import { useNodeStore } from 'src/stores/node';
 import type { AnalyticsRuleSchema } from 'typesense/lib/Typesense/AnalyticsRule';
-import { computed, onMounted, reactive } from 'vue';
+import { computed, onMounted, reactive, ref } from 'vue';
 import type { QTableProps } from 'quasar';
 import { useQuasar } from 'quasar';
+import MonacoEditor from 'src/components/MonacoEditor.vue';
 
 const $q = useQuasar();
 const store = useNodeStore();
@@ -123,6 +154,7 @@ function initialData(): AnalyticsRuleSchema {
       destination: {
         collection: '',
       },
+      expand_query: false,
       limit: 100,
     },
   };
@@ -177,9 +209,34 @@ const state = reactive({
 });
 
 const collectionNames = computed(() => store.data.collections.map((collection) => collection.name));
+const aliasNames = computed(() => store.data.aliases.map((alias) => alias.name));
+// Combine and sort collections and aliases for dropdowns
+const sourceOptions = computed(() => {
+  const options = [...collectionNames.value, ...aliasNames.value];
+  return options.sort();
+});
+
+const typeOptions = ref(['popular_queries', 'nohits_queries', 'counter']);
+
 const isUpdate = computed(() =>
   store.data.analyticsRules.map((a) => a.name).includes(state.rule.name),
 );
+
+const tab = ref('form');
+const ruleJson = computed({
+  get() {
+    return JSON.stringify(state.rule, null, 2);
+  },
+  set(json: string) {
+    try {
+      state.rule = JSON.parse(json);
+      jsonError.value = null;
+    } catch (e) {
+      jsonError.value = (e as Error).message;
+    }
+  },
+});
+const jsonError = ref<string | null>(null);
 
 async function createRule() {
   await store.createAnalyticsRule(state.rule);
